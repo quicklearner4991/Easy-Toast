645d86fbb908179edc8511adff7f45625e01ed72
Quicklearner4991 <>
2022-01-10 12:33:37 +0530

    Change:Method updated

--- easytoast/src/main/java/com/easytoast/lib/EasyToast.kt
+++ easytoast/src/main/java/com/easytoast/lib/EasyToast.kt
@@ -2,8 +2,11 @@ package com.easytoast.lib
 
 import android.content.Context
 import android.widget.Toast
+
 class EasyToast {
-    fun showToast(c: Context?, message: String?) {
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    companion object {
+        fun showToast(c: Context?, message: String?) {
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
